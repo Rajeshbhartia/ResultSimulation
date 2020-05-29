ad8eb887ee8a58c6e2a6d50a9a41823135a04772
rajesh bhartia <rbhartiamuk@gmmail.com>
2020-05-29 21:08:56 +0600

    multiple result showing

--- src/App.js
+++ src/App.js
@@ -2,64 +2,103 @@ import React, { Component } from 'react';
 import FileInput from './FIleInput';
 import PdfGeneratorInterface from "./PdfGeneratorInterface";
 import { Button } from '@material-ui/core';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class App extends Component {
 
   state = {
     tableHeadersArr: [],
     tableRowsArr: [],
-    isBtnDis: true
+    isBtnDis: true,
+    payload: {}
   }
 
   getData = (data) => {
-
-    data.forEach((item, i) => {
-      switch (true) {
-        case (item.Marks < 40):
-          item.grade = "fail"
-          break;
-        case (item.Marks < 50):
-          item.grade = "D"
-          break;
-        case (item.Marks < 60):
-          item.grade = "C"
-          break;
-        case (item.Marks < 70):
-          item.grade = "B"
-          break;
-        case (item.Marks < 80):
-          item.grade = "A"
-          break;
-        default:
-          item.grade = "A+"
-          break;
-      }
-    })
-
-
+    // console.log(this.state.payload)
     this.setState({
       tableHeadersArr: Object.keys(data[0]),
       tableRowsArr: data,
-      isBtnDis: false
     })
   }
 
   generatePDF = () => {
+
+    console.log(this.state.payload)
+
+    // let tableHeadersArr = [...this.state.tableHeadersArr];
+    let tableRowsArr = [...this.state.tableRowsArr];
+
+    Object.keys(this.state.payload).forEach((data) => {
+      tableRowsArr.forEach((item, index) => {
+        switch (true) {
+          case (item[data] < 40):
+            item["grade" + data] = "fail"
+            break;
+          case (item[data] < 50):
+            item["grade" + data] = "D"
+            break;
+          case (item[data] < 60):
+            item["grade" + data] = "C"
+            break;
+          case (item[data] < 70):
+            item["grade" + data] = "B"
+            break;
+          case (item[data] < 80):
+            item["grade" + data] = "A"
+            break;
+          default:
+            item["grade" + data] = "A+"
+            break;
+        }
+      })
+    })
+
     let pdfInterface = new PdfGeneratorInterface(
-      this.state.tableHeadersArr,
-      this.state.tableRowsArr,
+      Object.keys(tableRowsArr[0]),
+      tableRowsArr,
       "Result"
     );
     pdfInterface.downloadTableAsPDF();
   };
 
+  handleChange = (e) => {
+    let payload = { ...this.state.payload }
+    payload[e.target.name] = e.target.checked
+    this.setState({
+      payload,
+      isBtnDis: false
+    })
+  }
+
   render() {
     return (
       <div style={{ display: "flex", alignItems: "center", justifyContent: "center", background: "#ededed", minHeight: "100vh", flexDirection: "column" }}>
         <div style={{ padding: "20px" }}>
-
           <FileInput getData={this.getData} />
         </div>
+
+        {this.state.tableHeadersArr.length > 0 && (
+          <div>
+            <div>
+              Choose columns which you want for calculate grade:
+            </div>
+            <FormGroup row>
+              {this.state.tableHeadersArr.map((item, i) => {
+                return (
+                  <FormControlLabel
+                    control={<Checkbox checked={this.state.payload.item} onChange={this.handleChange} name={item} />}
+                    label={item}
+                    key={i}
+                  />
+                )
+              })}
+
+            </FormGroup>
+          </div>
+        )}
+
         <Button
           variant="contained"
           color="primary"
